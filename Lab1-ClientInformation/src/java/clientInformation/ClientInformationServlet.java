155018a00f19271b778fec8f72e585abe12aeed9
Samira <samira@Malcolm-the-MacBook.local>
2013-09-15 13:18:30 -0400

    add route annotation

--- Lab1-ClientInformation/src/java/clientInformation/ClientInformationServlet.java
+++ Lab1-ClientInformation/src/java/clientInformation/ClientInformationServlet.java
@@ -10,11 +10,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.annotation.*;
 /**
  *
  * @author Samira El-Rayyes 6439366
  */
+@WebServlet(name = "ClientInformationServlet", urlPatterns = {"/validate"})
 public class ClientInformationServlet extends HttpServlet {
 
     /**
